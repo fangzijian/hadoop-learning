6e8a605615b1a8009797b8b7f8450449c6fe7397
[80326288] <[fangzijian@oppo.com]>
2021-06-24 17:16:27 +0800

    sql

--- spark-learning/src/main/scala/com/fzj/spark/learning/TestScala.scala
+++ spark-learning/src/main/scala/com/fzj/spark/learning/TestScala.scala
@@ -1,7 +1,27 @@
 package com.fzj.spark.learning
 
+import com.fzj.spark.learning.cases.Domain
+
 object TestScala {
   def main(args: Array[String]): Unit = {
+    //模式匹配
+    val domain1  = Domain(1, 111, "name1", "field1")
+    val domain2  = Domain(2, 222, "name2", "field1")
+    val domain3  = Domain(3, 333, "name3", "field1")
+    for (domain <- List(domain1, domain2, domain3)) {
+      domain match {
+        case Domain(1, 111, "name1", "field1") => println(domain.name)
+        case Domain(2, 222, "name2", "field1") => println(domain.field)
+        case Domain(_, id2, name, age) =>
+          println(domain.ID, domain.id, domain.name)
+      }
+    }
+
     printf("hello world")
   }
+
+  //函数， 闭包，一个实现了 trait 的对象
+  var testFunction: Int => Int = (i: Int) => i * 10
+  //方法
+  def testMethod(i: Int): Int = i * 11
 }
